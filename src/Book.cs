0a9b070cfd71a29c040470bee3657a42a85b922f
Whysayme <ruslango44149@gmail.com>
2026-05-04 10:26:52 +0200

    Delete src/Book.cs

--- src/Book.cs
+++
@@ -1,8 +0,0 @@
-public class Book
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int Year { get; set; }
-    public bool IsAvailable { get; set; }
-}
