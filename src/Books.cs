e6d9943f087e3e05a036927d38aa0104d5d9abe3
Whysayme <ruslango44149@gmail.com>
2026-05-04 10:27:06 +0200

    Create Books.cs

---
+++ src/Books.cs
@@ -0,0 +1,8 @@
+public class Books
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public bool IsAvailable { get; set; }
+}
